95c7320274ceec90865a88678182624d02f2b834
ahmaddzulkarnainyoo-dev <ahmaddzulkarnainyoo-dev@users.noreply.github.com>
2026-07-14 18:39:13 +0700

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'core/theme/app_theme.dart';
 import 'core/theme/app_colors.dart';
+import 'firebase_options.dart';
 import 'screens/tos_screen.dart';
 
 // NOTE UNTUK CLINE/DEEPSEEK:
@@ -10,7 +12,9 @@ import 'screens/tos_screen.dart';
 // - Screen di bawah ini SHELL/placeholder saja: Splash, ToS, Login, Home kosong.
 //   Isi UI form-nya boleh dikerjakan di sini, logic auth JANGAN disentuh dulu.
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const WinatraApp());
 }
 
